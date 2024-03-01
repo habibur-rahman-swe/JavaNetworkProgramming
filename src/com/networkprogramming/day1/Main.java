94b84267ae047d11acc720b7703b2ff663e8d2f1
habibur-rahman-swe <habibur0199mhr@gmail.com>
2024-03-01 23:14:06 +0600

    Accessing Website Codes using Buffered Reader

--- src/com/networkprogramming/day1/Main.java
+++ src/com/networkprogramming/day1/Main.java
@@ -1,17 +1,33 @@
 package com.networkprogramming.day1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		InetAddress ocsaly = InetAddress.getByName("www.ocsaly.com");
 
-		System.out.println("Host Address: " + ocsaly.getHostAddress());
-		System.out.println("Cononcial Host Name: " + ocsaly.getCanonicalHostName());
-		System.out.println("Host name : " + ocsaly.getHostName());
-		ocsaly.isReachable(1000);
+		
+		try {
+			
+			URL url = new URL("https://ocsaly.com");
+			URLConnection myUrlConnection = url.openConnection();
+			BufferedReader br = new BufferedReader(new InputStreamReader(myUrlConnection.getInputStream()));
+			String myLine;
+			
+			while ((myLine = br.readLine()) != null) {
+				System.out.println(myLine);
+			}
+			
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());;
+		}
 
 	}
 }
